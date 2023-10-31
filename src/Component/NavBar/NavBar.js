1fd7233238cd555a3a1fbfa5f454b293f48f9440
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-10-31 14:25:58 +0530

    logout

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -8,37 +8,36 @@ import { getUserProfile } from '../../Api/Api';
 function NavBar() {
   const [isResumeHovered, setIsResumeHovered] = useState(false);
   const [isCoverLetterHovered, setIsCoverLetterHovered] = useState(false);
-const authToken=JSON.parse(localStorage.getItem("token"))
-const [user,setUser] = useState([])
+  const [isUserHovered, setIsUserHovered] = useState(false);
+  const authToken = JSON.parse(localStorage.getItem("token"))
+  const [user, setUser] = useState([])
 
+  useEffect(() => {
+    handleuserProfile();
+  }, []);
 
-useEffect(() => {
-  handleuserProfile();
-}, []);
+  const handleuserProfile = async () => {
+    try {
+      const response = await getUserProfile();
 
-const  handleuserProfile= async () => {
-  try {
-    const response = await getUserProfile();
-
-    if (response.status === true) {
-      setUser(response.data)
-    } else {
-      console.error('Error fetching categories:', response.data.message);
+      if (response.status === true) {
+        setUser(response.data)
+      } else {
+        console.error('Error fetching user profile:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching user profile:', error.message);
     }
-  } catch (error) {
-    console.error('Error fetching categories:', error.message);
+  };
+
+  const handleLogOut = () => {
+    localStorage.clear()
   }
-};
-
-const handleLogOut=()=>{
-  localStorage.clear()
-}
-
-
 
   const handleResumeHover = () => {
     setIsResumeHovered(true);
     setIsCoverLetterHovered(false); // Close the Cover Letter dropdown
+    setIsUserHovered(false); 
   };
 
   const handleResumeLeave = () => {
@@ -48,10 +47,21 @@ const handleLogOut=()=>{
   const handleCoverLetterHover = () => {
     setIsCoverLetterHovered(true);
     setIsResumeHovered(false); // Close the Resume dropdown
+    setIsUserHovered(false); 
   };
 
   const handleCoverLetterLeave = () => {
     setIsCoverLetterHovered(false);
+  };
+
+  const handleUserHover = () => {
+    setIsUserHovered(true);
+    setIsCoverLetterHovered(false); 
+    setIsResumeHovered(false);
+  };
+
+  const handleUserLeave = () => {
+    setIsUserHovered(false); // Close the user dropdown
   };
 
   return (
@@ -64,13 +74,12 @@ const handleLogOut=()=>{
         <h4
           className={isResumeHovered ? style.active : style.notActive}
           onMouseEnter={handleResumeHover}
-       
         >
           Resume
         </h4>
 
         {isResumeHovered && (
-          <div className={style.servicesDiv}    onMouseLeave={handleResumeLeave}>
+          <div className={style.servicesDiv} onMouseLeave={handleResumeLeave}>
             <span className={style.arrow}>▲</span>
             <ServicesOption />
           </div>
@@ -78,29 +87,36 @@ const handleLogOut=()=>{
         <h4
           className={isCoverLetterHovered ? style.active : style.notActive}
           onMouseEnter={handleCoverLetterHover}
-        
         >
           Cover Letter
         </h4>
 
         {isCoverLetterHovered && (
-          <div className={style.servicesDiv}   onMouseLeave={handleCoverLetterLeave}>
+          <div className={style.servicesDiv} onMouseLeave={handleCoverLetterLeave}>
             <span className={style.arrow1}>▲</span>
             <ServicesOption />
           </div>
         )}
-{
-  authToken ? 
-  <>
-  {user?.name}
-  <button onClick={handleLogOut}>Log Out</button>
-  </> 
-   :
-    <Link to={"/SignIn"}><h4>Sign In</h4></Link>
-}
+        {authToken ? (
+          <>
+            <h3 className={style.user} onMouseEnter={handleUserHover}>
+              {user?.name}
+            </h3>
+            {isUserHovered && (
+              <div className={style.logout_box} onMouseLeave={handleUserLeave}>
+                <p onClick={handleLogOut}>Log Out</p>
+              </div>
+            )}
+          </>
+        ) : (
+          <Link to={"/SignIn"}>
+            <h4>Sign In</h4>
+          </Link>
+        )}
 
-<Link to={"/SignUp"}><h4>Sign Up</h4></Link>
-      
+        <Link to={"/SignUp"}>
+          <h4>Sign Up</h4>
+        </Link>
       </div>
     </div>
   );
