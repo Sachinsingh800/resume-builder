a7cd58b34e9bf26e3a1bdd964116c9818e0ebd08
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-11-09 11:41:01 +0530

    resume templates

--- src/Component/Carsouel/CardTempletes.js
+++ src/Component/Carsouel/CardTempletes.js
@@ -4,6 +4,33 @@ import style from "./CardTempletes.module.css"
 import resume_1 from "../Images/Template_1.png"
 import resume_2 from "../Images/Template_2.png"
 import resume_3 from "../Images/Template_3.png"
+import resume_4 from "../TemplateImages/1.jpg"
+import resume_5 from "../TemplateImages/2.jpg"
+import resume_6 from "../TemplateImages/3.jpg"
+import resume_7 from "../TemplateImages/4.jpg"
+import resume_8 from "../TemplateImages/5.jpg"
+import resume_9 from "../TemplateImages/6.jpg"
+import resume_10 from "../TemplateImages/7.jpg"
+import resume_11 from "../TemplateImages/8.jpg"
+import resume_12 from "../TemplateImages/9.jpg"
+import resume_13 from "../TemplateImages/10.jpg"
+import resume_14 from "../TemplateImages/11.jpg"
+import resume_15 from "../TemplateImages/12.jpg"
+import resume_16 from "../TemplateImages/13.jpg"
+import resume_17 from "../TemplateImages/14.jpg"
+import resume_18 from "../TemplateImages/15.jpg"
+import resume_19 from "../TemplateImages/16.jpg"
+import resume_20 from "../TemplateImages/17.jpg"
+import resume_21 from "../TemplateImages/18.jpg"
+import resume_22 from "../TemplateImages/19.jpg"
+import resume_23 from "../TemplateImages/20.jpg"
+import resume_24 from "../TemplateImages/21.jpg"
+import resume_25 from "../TemplateImages/22.jpg"
+import resume_26 from "../TemplateImages/23.jpg"
+import resume_27 from "../TemplateImages/24.jpg"
+import resume_28 from "../TemplateImages/25.jpg"
+import resume_29 from "../TemplateImages/26.jpg"
+import resume_30 from "../TemplateImages/27.jpg"
 import { useRecoilState } from "recoil";
 import { chooseTemplates } from '../../Recoil' 
 
@@ -33,6 +60,330 @@ export function CardTempletes3() {
     <div onClick={()=>setTemplateNo(2)}  className={style.main}>
          
          <img src={resume_3} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes4() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_4} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes5() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_5} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes6() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_6} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes7() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_7} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes8() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_8} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes9() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_9} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes10() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_10} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes11() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_11} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes12() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_12} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes13() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_13} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes14() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_14} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes15() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_15} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes16() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_16} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes17() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_17} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes18() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_18} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes19() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_19} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes20() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_20} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes21() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_3} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes22() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_22} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes23() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_23} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes24() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_24} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes25() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_25} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes26() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_26} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes27() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_27} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes28() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_28} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes29() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_29} alt='resume2' />
+
+
+   
+    </div>
+  )
+}
+export function CardTempletes30() {
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  return (
+    <div onClick={()=>setTemplateNo(2)}  className={style.main}>
+         
+         <img src={resume_30} alt='resume2' />
 
 
    
