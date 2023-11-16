bf74846b68e4e46b354b7d3f708198ad41531322
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-11-17 02:52:27 +0530

    18

---
+++ src/Component/ResumeTemplates/Template_18/Template_18.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { Divider } from "@mui/material";
+import style from "./Template_18.module.css";
+import dp from "../../Images/dp.png"
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
+import PlaceIcon from '@mui/icons-material/Place';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Template_18 = () => {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+      <div>
+             <div className={style.img_box}>
+                <img src={dp} alt="dp" />
+             </div>
+           </div>
+        <div className={style.name_box}>
+        <h1 className={style.name}>JESSICA CLAIRE</h1>
+        <h5 className={style.name}>frontend Developer</h5>
+        </div>
+        
+      </div>
+      <div className={style.container}>
+      
+        <div
+          className={style.right_section}
+        
+        >
+                <div className={style.section}>
+       <h2 className={style.section_title}>EDUCATION</h2>
+       <Divider className="divider" />
+
+       <div className={style.work_entry}>
+       <p className="date">2019.08 - 2023.09</p>
+
+       <div>
+       <h3 className="degree">Masters in Data Science</h3>
+         <p className="university">ABC College</p>
+ 
+       </div>
+    
+
+       </div>
+     </div>
+           
+            <br/>
+          <h2 className="section-title">CONTACTS</h2>
+          <Divider className="divider" />
+          <div className={style.contact_info}>
+     
+            <div className={style.contact_value}>
+              <span className={style.contact_label}>< LocalPhoneIcon  sx={{fontSize:"20px"}} /></span>
+              <p className="contact-value">+91 9503942697</p>
+            </div>
+            <div className={style.contact_value}>
+            <span className={style.contact_label}>< EmailIcon sx={{fontSize:"20px"}}  /></span>
+              <p className="contact-value">ss20010126@gmail.com</p>
+            </div>
+
+            <div className={style.contact_value}>
+            <span className={style.contact_label}>< LinkedInIcon sx={{fontSize:"20px"}} /></span>
+              <p className="contact-value">linkedin.com/en/5hubzzz</p>
+            </div>
+            <div className={style.contact_value}>
+              <span className={style.contact_label}><PlaceIcon sx={{fontSize:"20px"}} /></span>
+              <p className="contact-value">Enter Your Address here</p>
+            </div>
+          </div>
+
+
+          <br/>
+
+          <div className="section">
+            <h2 className="section-title">SKILLS</h2>
+            <Divider className="divider" />
+            <ul className={style.skills_list}>
+              <li>javascript </li>
+              <li>javascript </li>
+              <li>javascript </li>
+              <li>javascript </li>
+              <li>javascript </li>
+              <li>javascript </li>
+      
+       
+            </ul>
+          </div>
+          <br/>
+          <div className="section">
+            <h2 className="section-title">LANGUAGE</h2>
+            <Divider className="divider" />
+            <ul className={style.skills_list}>
+              <li>Hindi</li>
+              <li>English</li>
+              <li>Urdu</li>
+            </ul>
+          </div>
+          <br/>
+          <div className="section">
+            <h2 className="section-title">INTREST</h2>
+            <Divider className="divider" />
+            <ul className={style.skills_list}>
+              <li>Hindi</li>
+              <li>English</li>
+              <li>Urdu</li>
+            </ul>
+          </div>
+        </div>
+        <div className={style.left_section}>
+        <div className="section">
+            <h2 className="section-title">SUMMARY</h2>
+            <Divider className="divider" />
+            <p className="section-content">
+              Lorem Ipsum is simply dummy text of scrambled it to make a ty It
+              was popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more.
+            </p>
+          </div>
+     <div className={style.section}>
+       <h2 className={style.section_title}>EXPERIENCE</h2>
+       <Divider className="divider" />
+
+       <div className={style.work_entry}>
+       
+
+         <div>
+            <div className={style.title_}>
+            <h3 className="position">Software Engineer</h3>
+            <p className="date">2019.08 - Present</p>
+            </div>
+
+           <p className="company">ABC Company</p>
+           <p className={style.description}>
+             Lorem Ipsum is simply dummy text of Lorem Ipsum passages, and
+             Aldus PageMaker including versions of Lorem Ipsum.
+           </p>
+         </div>
+
+
+       </div>
+     </div>
+ 
+
+   </div>
+      </div>
+    </div>
+  );
+};
+
+export default Template_18;
--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ import Template_14 from './Component/ResumeTemplates/Template_14/Template_14';
 import Template_15 from './Component/ResumeTemplates/Template_15/Template_15';
 import Template_16 from './Component/ResumeTemplates/Template_16/Template_16';
 import Template_17 from './Component/ResumeTemplates/Template_17/Template_17';
+import Template_18 from './Component/ResumeTemplates/Template_18/Template_18';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
@@ -131,6 +132,10 @@ const router = createBrowserRouter([
   {
     path: "/Template_17",
     element: <Template_17 />,
+  },
+  {
+    path: "/Template_18",
+    element: <Template_18 />,
   },
 
   {
