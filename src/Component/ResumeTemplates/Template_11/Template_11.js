c8d4dd94ed1726998d471475cbbfd184fc975022
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-11-19 23:55:29 +0530

    12

--- src/Component/ResumeTemplates/Template_11/Template_11.js
+++ src/Component/ResumeTemplates/Template_11/Template_11.js
@@ -8,16 +8,16 @@ import { MdOutlineWorkHistory } from "react-icons/md";
 import dp from "../../Images/dp.png";
 import ProgressBar from "../../ProgressBar/ProgressBar";
 import { useRecoilState } from "recoil";
-import { 
+import {
   ChooseColor,
   chooseTemplates,
   ChooseColorSecond,
-  croppedImageState ,
-  resumeData, 
+  croppedImageState,
+  resumeData,
   ChooseColorThird,
   fontState,
   fontSizeState,
-  imageSizeState
+  imageSizeState,
 } from "../../../Recoil";
 
 const Template_11 = () => {
@@ -31,130 +31,136 @@ const Template_11 = () => {
   const [croppedImage, setCroppedImage] = useRecoilState(croppedImageState);
   const [formData, setFormData] = useRecoilState(resumeData);
 
-  console.log(formData.resume,"resume data")
+  console.log(formData.resume, "resume data");
 
   const handleDate = (data) => {
-    console.log(data,"data")
+    console.log(data, "data");
 
     const startYear = new Date(data).getFullYear();
-  
-    return startYear
+
+    return startYear;
   };
   return (
-    <div onClick={()=>setTemplateNo(2)} className={style.main}>
+    <div onClick={() => setTemplateNo(2)} className={style.main}>
       <div
         className={style.Left_container}
         style={{ backgroundColor: color, color: "white" }}
       >
         <div className={style.objectiveHeader}>
-          <h1 className={style.person_name}  style={{color:color2,fontFamily:fontStyle,fontSize:fontSize}}>
-          {formData?.resume?.name}
+          <h1
+            className={style.person_name}
+            style={{ color: color2, fontFamily: fontStyle, fontSize: fontSize }}
+          >
+            {formData?.resume?.name}
           </h1>
           <p className={style.objectiveText}> {formData?.resume?.jobTitle}</p>
         </div>
 
-
         <br />
 
         <div className={style.info_box}>
-          <h3 style={{ color: color3}}>Personal info</h3>
+          <h3 style={{ color: color3 }}>Personal info</h3>
 
           <div className={style.contactInfo}>
             <label>E-mail</label>
-            <p className={style.email} style={{ color: color3}}>{formData?.resume?.contact?.email}</p>
+            <p className={style.email} style={{ color: color3 }}>
+              {formData?.resume?.contact?.email}
+            </p>
           </div>
           <div className={style.contactInfo}>
-          <label>Phone</label>
-            <p style={{ color: color3}} className={style.email}>{formData?.resume?.contact?.phone}</p>
+            <label>Phone</label>
+            <p style={{ color: color3 }} className={style.email}>
+              {formData?.resume?.contact?.phone}
+            </p>
           </div>
 
           <div className={style.contactInfo}>
-          <label>Address</label>
-            <p className={style.email} style={{ color: color3}}>{formData?.resume?.address?.address},{formData?.resume?.address?.postalCode}</p>
+            <label>Address</label>
+            <p className={style.email} style={{ color: color3 }}>
+              {formData?.resume?.address?.address},
+              {formData?.resume?.address?.postalCode}
+            </p>
           </div>
         </div>
-        <br/>
-     
+        <br />
+
         <div className={style.skillsHeader}>
-          <h3 style={{ color: color3}}>Additional Skills</h3>
+          <h3 style={{ color: color3 }}>Additional Skills</h3>
 
           <ul>
-          {formData?.resume?.skillsAndLevel.map((item,id)=>
-           <li  key={id} style={{ color: color3}}>
-          <span>{item.skills}</span>  
-          <p className={style.ProgressBar}> <ProgressBar bgcolor="orange"  height={5}  /></p>
-
-             </li>
-          )}
+            {formData?.resume?.skillsAndLevel.map((item, id) => (
+              <li key={id} style={{ color: color3 }}>
+                <span>{item.skills}</span>
+                <p className={style.ProgressBar}>
+                  {" "}
+                  <ProgressBar bgcolor="orange" height={5} />
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
-        <br/>
+        <br />
         <div className={style.skillsHeader}>
-          <h3 style={{ color: color3}}>LAGNUAGES</h3>
+          <h3 style={{ color: color3 }}>LAGNUAGES</h3>
 
           <ul>
-          {formData?.resume?.knownLanguages.map((item,id)=>
-           <li  key={id} style={{ color: color3}}>
-          <span>{item.lang}</span>  
-          <p className={style.ProgressBar}>          <ProgressBar bgcolor="orange"  height={5} /> </p>
-
-             </li>
-          )}
+            {formData?.resume?.knownLanguages.map((item, id) => (
+              <li key={id} style={{ color: color3 }}>
+                <span>{item.lang}</span>
+                <p className={style.ProgressBar}>
+                  {" "}
+                  <ProgressBar bgcolor="orange" height={5} />{" "}
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
       <div>
-
-
         <div className={style.skillsHeader2}>
-          <h2>
-            Skills Summary
-          </h2>
+          <h2>Skills Summary</h2>
 
-          <p>
-          {formData?.resume?.summary}
-          </p>
+          <p>{formData?.resume?.summary}</p>
         </div>
 
         <div className={style.professionalSkillsHeader}>
           <div>
-          <h2>EXPERIENCE</h2>
-      
-
+            <h2>EXPERIENCE</h2>
           </div>
-  
-    
+
           <ul>
-          {formData?.resume?.work.map((item,id)=>
-          <li>
-          <div className={style.work_des}>
-        <h3 className={style.customerService}>{item?.title}</h3>
-        <h5 className={style.company_name}><span>{item?.company} - {item?.location}</span> <span>{handleDate(item?.startDate)} - {handleDate(item?.endDate)}</span></h5>
-          <p>
-              {item?.description}
-          </p>
-     
-          </div>
-          </li>
-          )}
-            
+            {formData?.resume?.work.map((item, id) => (
+              <li>
+                <div className={style.work_des}>
+                  <h3 className={style.customerService}>{item?.title}</h3>
+                  <h5 className={style.company_name}>
+                    <span>
+                      {item?.company} - {item?.location}
+                    </span>{" "}
+                    <span>
+                      {handleDate(item?.startDate)} -{" "}
+                      {handleDate(item?.endDate)}
+                    </span>
+                  </h5>
+                  <p>{item?.description}</p>
+                </div>
+              </li>
+            ))}
           </ul>
-       
-         
-          
-
         </div>
         <div className={style.skillsHeader2}>
-          <h2 style={{ color: color3}}>Education</h2>
+          <h2 style={{ color: color3 }}>Education</h2>
 
           <ul className={style.education}>
-          {formData?.resume?.education.map((item,id)=>
-           <li  key={id} style={{ color: color3}}>
-          <h4>{item.degree}</h4>  
-          <span>{item?.startYear}  - {item.endYear}</span>
-          <span>{item.collegeName}</span>  
-             </li>
-          )}
+            {formData?.resume?.education.map((item, id) => (
+              <li key={id} style={{ color: color3 }}>
+                <h4>{item.degree}</h4>
+                <span>
+                  {item?.startYear} - {item.endYear}
+                </span>
+                <span>{item.collegeName}</span>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/Component/ResumeTemplates/Template_12/Template_12.js
+++ src/Component/ResumeTemplates/Template_12/Template_12.js
@@ -1,130 +1,221 @@
 import React from "react";
 import { Divider } from "@mui/material";
 import style from "./Template_12.module.css";
+import { useRecoilState } from "recoil";
+import { 
+  ChooseColor,
+  chooseTemplates,
+  ChooseColorSecond,
+  croppedImageState ,
+  resumeData, 
+  ChooseColorThird,
+  fontState,
+  fontSizeState,
+  imageSizeState
+} from "../../../Recoil";
 
 const Template_12 = () => {
+  const [color, setColor] = useRecoilState(ChooseColor);
+  const [color2, setColor2] = useRecoilState(ChooseColorSecond);
+  const [color3, setColor3] = useRecoilState(ChooseColorThird);
+  const [fontStyle, setFontStyle] = useRecoilState(fontState);
+  const [fontSize, setFontSize] = useRecoilState(fontSizeState);
+  const [imgSize, setImgSize] = useRecoilState(imageSizeState);
+  const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
+  const [croppedImage, setCroppedImage] = useRecoilState(croppedImageState);
+  const [formData, setFormData] = useRecoilState(resumeData);
+
+  console.log(formData.resume,"resume data")
+
+  const handleDate = (data) => {
+    console.log(data,"data")
+   
+
+    const startYear = new Date(data).getFullYear();
+  
+    return startYear
+  };
   return (
     <div className={style.main}>
       <div className={style.header}>
-        <h1 className={style.name}>JESSICA CLAIRE</h1>
-        <h5 className={style.name}>frontend Developer</h5>
+        <h1 className={style.name}>{formData?.resume?.name}</h1>
+        <h5 className={style.name}> {formData?.resume?.jobTitle}</h5>
       </div>
       <div className={style.container}>
         <div className={style.left_section}>
           <div className="section">
             <p className="section-content">
-              Lorem Ipsum is simply dummy text of scrambled it to make a ty It
-              was popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more.
+            {formData?.resume?.summary}
             </p>
           </div>
           <div className={style.section}>
             <h2 className="section-title">Experience</h2>
             <Divider className="divider" />
-
+            <ul>
+            {formData?.resume?.work.map((item, id) => (
+              <li>
             <div className={style.work_entry}>
-              <p className="date">2019.08 - Present</p>
+              
+              <p className="date">  {handleDate(item?.startDate)}  -  {handleDate(item?.endDate)}</p>
               <div>
-                <h3 className="position">Software Engineer</h3>
-                <p className="company">ABC Company</p>
+                <h3 className="position">{item?.title}</h3>
+                <p className="company">{item?.company} - {item?.location}</p>
 
                 <p className="description">
-                  Lorem Ipsum is simply dummy text of Lorem Ipsum passages, and
-                  Aldus PageMaker including versions of Lorem Ipsum.
+                {item?.description}
                 </p>
               </div>
             </div>
+              </li>
+            ))}
+          </ul>
+            <ul>
+            {formData?.resume?.work.map((item, id) => (
+              <li>
+            <div className={style.work_entry}>
+              
+              <p className="date">  {handleDate(item?.startDate)}  -  {handleDate(item?.endDate)}</p>
+              <div>
+                <h3 className="position">{item?.title}</h3>
+                <p className="company">{item?.company} - {item?.location}</p>
+
+                <p className="description">
+                {item?.description}
+                </p>
+              </div>
+            </div>
+              </li>
+            ))}
+          </ul>
+            <ul>
+            {formData?.resume?.work.map((item, id) => (
+              <li>
+            <div className={style.work_entry}>
+              
+              <p className="date">  {handleDate(item?.startDate)}  -  {handleDate(item?.endDate)}</p>
+              <div>
+                <h3 className="position">{item?.title}</h3>
+                <p className="company">{item?.company} - {item?.location}</p>
+
+                <p className="description">
+                {item?.description}
+                </p>
+              </div>
+            </div>
+              </li>
+            ))}
+          </ul>
+
+
+   
           </div>
           <div className={style.section}>
             <h2 className="section-title">EDUCATION</h2>
             <Divider className="divider" />
-            <div className="education-entry">
-              <h3 className="degree">Masters in Data Science</h3>
-              <p className="university">ABC College</p>
-              <p className="date">2019.08 - 2023.09</p>
-              <p className="description">
-                Lorem Ipsum is simply dummy text of Lorem Ipsum passages, and
-                Aldus PageMaker including versions of Lorem Ipsum.
-              </p>
+            <ul className={style.education}>
+            {formData?.resume?.education.map((item, id) => (
+              <>
+                 <li key={id} style={{ color: color3 }}>
+           <div className="education-entry">
+              <h3 className="degree">{item.degree}</h3>
+              <p className="university">{item.collegeName}</p>
+              <p className="date">{item?.startYear} - {item.endYear}</p>
+      
             </div>
+              </li>
+                 <li key={id} style={{ color: color3 }}>
+           <div className="education-entry">
+              <h3 className="degree">{item.degree}</h3>
+              <p className="university">{item.collegeName}</p>
+              <p className="date">{item?.startYear} - {item.endYear}</p>
+      
+            </div>
+              </li>
+              </>
+           
+            ))}
+          </ul>
+        
           </div>
           <div className={style.section}>
             <h2 className="section-title">Certification</h2>
             <Divider className="divider" />
-            <div className="education-entry">
-              <h3 className="degree">Masters in Data Science</h3>
-              <p className="university">ABC College</p>
-              <p className="date">2019.08 - 2023.09</p>
-              <p className="description">
-                Lorem Ipsum is simply dummy text of Lorem Ipsum passages, and
-                Aldus PageMaker including versions of Lorem Ipsum.
-              </p>
+            <ul className={style.education}>
+            {formData?.resume?.certifications.map((item, id) => (
+              <li key={id} style={{ color: color3 }}>
+           <div className="education-entry">
+              <h3 className="degree">{item.title}</h3>
+              <p className="university">{item.issuingOrganization}</p>
+              <p className="date">{item?.date}</p>
+      
             </div>
+              </li>
+            ))}
+          </ul>
+        
           </div>
         </div>
         <div
           className={style.right_section}
-          style={{ backgroundColor: "grey" }}
+          style={{ backgroundColor: "#D3D3D3" }}
         >
           <h2 className="section-title">Personal Info</h2>
           <Divider className="divider" />
           <div className="contact-info">
             <div className="contact-entry">
               <h4 className="contact-label">Address</h4>
-              <p className="contact-value">Enter Your Address here</p>
+              <p className="contact-value">         {formData?.resume?.address?.address},
+              {formData?.resume?.address?.postalCode}</p>
             </div>
             <div className="contact-entry">
               <h4 className="contact-label">Phone</h4>
-              <p className="contact-value">+91 9503942697</p>
+              <p className="contact-value">  {formData?.resume?.contact?.phone}</p>
             </div>
             <div className="contact-entry">
               <h4 className="contact-label">E-mail</h4>
-              <p className="contact-value">ss20010126@gmail.com</p>
-            </div>
-            <div className="contact-entry">
-              <h4 className="contact-label">Twitter</h4>
-              <p className="contact-value">@5hubzzz</p>
+              <p className="contact-value">      {formData?.resume?.contact?.email}</p>
             </div>
             <div className="contact-entry">
               <h4 className="contact-label">LinkedIn</h4>
-              <p className="contact-value">linkedin.com/en/5hubzzz</p>
+              <p className="contact-value"> {formData?.resume?.socialLinks?.linkedin }</p>
             </div>
           </div>
 
           <div className="section">
             <h2 className="section-title">SKILLS</h2>
             <Divider className="divider" />
-            <ul className="skills-list">
+            <ul>
+            {formData?.resume?.skillsAndLevel.map((item, id) => (
               <li>
-                <h4>C++</h4>
-                <p>Advanced</p>
+                <h4>{item?.skills}</h4>
+                <p>{item?.level}</p>
               </li>
-              <li>
-                <h4>C++</h4>
-                <p>Advanced</p>
-              </li>
-              <li>
-                <h4>C++</h4>
-                <p>Advanced</p>
-              </li>
+            ))}
             </ul>
+     
           </div>
           <div className="section">
             <h2 className="section-title">Language</h2>
             <Divider className="divider" />
-            <ul className="skills-list">
-              <li>Hindi</li>
-              <li>English</li>
-              <li>Urdu</li>
+            <ul>
+            {formData?.resume?.knownLanguages.map((item, id) => (
+              <li>
+                <h4>{item?.lang}</h4>
+                </li>
+            ))}
             </ul>
           </div>
           <div className="section">
-            <h2 className="section-title">Interest</h2>
+            <h2 className="section-title">Awards</h2>
             <Divider className="divider" />
-            <ul className="skills-list">
-              <li>Hindi</li>
-              <li>English</li>
-              <li>Urdu</li>
+            <ul>
+            {formData?.resume?.awards.map((item, id) => (
+              <li>
+                <h5>{item?.date}</h5>
+                <h4>{item?.title}</h4>
+                <p>{item?.issuingOrganization}</p>
+                </li>
+            ))}
             </ul>
           </div>
         </div>
