d67e407750202b4929020c1f3f5b90af569641ee
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-12-01 21:30:12 +0530

    css

--- src/Component/ResumeTemplates/Template_1/Template_1.js
+++ src/Component/ResumeTemplates/Template_1/Template_1.js
@@ -1,23 +1,38 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+import location from "../../Images/location-pin.png"
+import linkedin from "../../Images/linkedin.png"
+import mail from "../../Images/mail.png"
+import call from "../../Images/call.png"
+import dp from "../../Images/dp2.jpg"
+import { Divider } from "@mui/material";
 import style from "./Template_1.module.css";
-import { AiOutlineMail } from "react-icons/ai";
-import { AiOutlinePhone } from "react-icons/ai";
-import { CiLocationOn } from "react-icons/ci";
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import PlaceIcon from "@mui/icons-material/Place";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import ProgressBar from "../../ProgressBar/ProgressBar";
 import { useRecoilState } from "recoil";
 import {
   ChooseColor,
   chooseTemplates,
   ChooseColorSecond,
-  resumeData,
   croppedImageState,
+  resumeData,
   ChooseColorThird,
   fontState,
   fontSizeState,
   imageSizeState,
 } from "../../../Recoil";
-import axios from "axios";
 
-function Template_1() {
+
+const PDFRenderer = ({ htmlContent }) => {
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+};
+
+const Template_1= () => {
   const [color, setColor] = useRecoilState(ChooseColor);
   const [color2, setColor2] = useRecoilState(ChooseColorSecond);
   const [color3, setColor3] = useRecoilState(ChooseColorThird);
@@ -25,120 +40,440 @@ function Template_1() {
   const [fontSize, setFontSize] = useRecoilState(fontSizeState);
   const [imgSize, setImgSize] = useRecoilState(imageSizeState);
   const [templateNo, setTemplateNo] = useRecoilState(chooseTemplates);
-  const [formData, setFormData] = useRecoilState(resumeData);
   const [croppedImage, setCroppedImage] = useRecoilState(croppedImageState);
+  const [formData, setFormData] = useRecoilState(resumeData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [base64Image1, setBase64Image1] = useState('');
+  const [base64Image2, setBase64Image2] = useState('');
+  const [base64Image3, setBase64Image3] = useState('');
+  const [base64Image4, setBase64Image4] = useState('');
+  const [base64Image5, setBase64Image5] = useState('');
+
+  console.log(formData.resume, "resume data");
+
+  const handleDate = (data) => {
+    console.log(data, "data");
+
+    const startYear = new Date(data).getFullYear();
+
+    return startYear;
+  };
+
+
+
+  
+  useEffect(() => {
+    const imageLocations = [
+      location,
+      linkedin,
+      dp,
+      mail,
+      call,
+    ];
+  
+    const handleImageChange = async () => {
+      try {
+        const promises = imageLocations.map(async (location, index) => {
+          const response = await fetch(location);
+          const blob = await response.blob();
+          const reader = new FileReader();
+  
+          return new Promise((resolve) => {
+            reader.onloadend = () => {
+              // The result property contains the base64-encoded string
+              const base64String = reader.result;
+              resolve({ index, base64String });
+            };
+  
+            // Read the image file as a data URL
+            reader.readAsDataURL(blob);
+          });
+        });
+  
+        // Wait for all promises to resolve
+        const results = await Promise.all(promises);
+  
+        // Update state based on index
+        results.forEach(({ index, base64String }) => {
+          if (index === 0) {
+            setBase64Image1(base64String);
+          } else if (index === 1) {
+            setBase64Image2(base64String);
+          }else if (index === 2) {
+            setBase64Image3(base64String);
+          }
+          else if (index === 3) {
+            setBase64Image4(base64String);
+        
+          }else if (index === 4) {
+            setBase64Image5(base64String);
+          }
+        });
+      } catch (error) {
+        console.error('Error loading images:', error);
+      }
+    };
+  
+    handleImageChange();
+  }, []);
+  
+
+  const getHTML = () => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css"> <!-- You may link your stylesheet if you have one -->
+        <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+            box-sizing: border-box;
+            background-color: white;
+        }
+        .main {
+            width: 850px;
+            height: 1130px;
+            background-color: white;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+        }
+        
+.img_box{
+    height: 7rem;
+    width: 7rem;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+}
+.img_box img{
+    height: 100%;
+    width: 100%;
+}
+.left_section{
+    display: flex;
+    flex-direction: column;
+    padding: 3rem 1rem;
+    height: 1112px;
+    text-align: left;
+
+}
+
+.info_box{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    padding: 1rem 1rem;
+}
+.education{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    padding: 1rem 1rem;
+}
+.img_container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.right_section{
+    display: flex;
+    flex-direction: column;
+ 
+}
+.right_section p{
+    width: 95%!important;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    list-style: none;
+   
+}
+.right_section ul li{
+    margin-left:1.5rem ;
+   
+}
+.right_section ul li {
+    width: 95%!important;
+}
+.work_history{
+    display: flex;
+    flex-direction: column;
+   
+}
+.heading{
+    background-color: aliceblue;
+    padding: 3rem 1rem;
+}
+.certifications{
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: .5rem;
+}
+.skills{
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+
+}
+.skills ul{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+   
+}
+.professional_summary{
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+}
+.work{
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+}
+hr{
+    margin-left: 1rem;
+}
+.info_box p{
+    display: flex;
+    gap: .5rem;
+    align-items: center;
+    margin:0rem;
+}
+.certifications ul{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+   
+}
+.edu{
+    flex-direction: column;
+    display: flex;
+    gap:.5rem;
+}
+.edu h4,p{
+    margin:0rem; 
+}
+.certi-ul {
+    margin-top:-1rem;
+    margin-left:-2.7rem;
+    flex-direction: column;
+    display: flex;
+    gap:.5rem;
+}
+.certi-ul li h5,p{
+    margin:0rem; 
+}
+.skill-ul{
+    margin-top:-1rem;
+    margin-left:-2.7rem;
+}
+.line{
+    margin-top:-1rem;
+    width:98%;
+}
+.work-ul{
+  
+    margin-top:-1rem;
+    margin-left:-2.7rem;
+    flex-direction: column;
+    display: flex;
+    gap:.5rem;
+    
+}
+.work-ul li h4,p{
+    margin:0rem;  
+}
+        </style>
+        <title>Your Page Title</title>
+    </head>
+    
+    <body>
+        <div class="main">
+            <div class="left_section" style="background-color: #3498db; color: white;">
+                <div>
+                    <div class="img_container">
+                        <div class="img_box" style="height: 150px; width: 150px;">
+                            <!-- Replace the image source with your actual 
+                            image URL -->
+                            <img src=${base64Image3} alt="dp" />
+                        </div>
+                    </div>
+    
+                    <div class="info_box">
+                        <p>
+                            <span>Email: </span>
+                            john.doe@example.com
+                        </p>
+                        <p>
+                            <span>Phone: </span>
+                            123-456-7890
+                        </p>
+                        <p>
+                            <span>Location: </span>
+                            123 Main Street, 56789
+                        </p>
+                    </div>
+                </div>
+               <div><hr /></div> 
+                <div class="education">
+                    <h3>Education</h3>
+                    <div class="edu">
+                    <div>
+                    <h4>University of Example</h4>
+                    <p>Master's Degree</p>
+                    <p>2015 - 2019</p>
+                    </div>
+                    <div>
+                    <h4>University of Example</h4>
+                    <p>Master's Degree</p>
+                    <p>2015 - 2019</p>
+                    </div>
+                       
+                    </div>
+                    <!-- Add more education items as needed -->
+                </div>
+            </div>
+    
+            <div class="right_section">
+                <div class="heading" style="background-color:#3498db;">
+                    <h1 style="color: white; font-family: 'Arial', sans-serif; ">
+                        John Doe
+                    </h1>
+                </div>
+    
+                <div class="certifications">
+                    <h3>Certifications</h3>
+                    <ul class="certi-ul">
+                        <li>
+                            <h5>Example Certification</h5>
+                            <p>Organization: Certification Organization</p>
+                        </li>
+                        <li>
+                            <h5>Example Certification</h5>
+                            <p>Organization: Certification Organization</p>
+                        </li>
+                        <li>
+                            <h5>Example Certification</h5>
+                            <p>Organization: Certification Organization</p>
+                        </li>
+                        <!-- Add more certification items as needed -->
+                    </ul>
+                </div>
+    
+             <div class="line"> <hr /></div>  
+    
+                <div class="skills">
+                    <h3>Skills</h3>
+                    <ul class="skill-ul">
+                        <li>Skill 1</li>
+                        <li>Skill 2</li>
+                        <!-- Add more skills as needed -->
+                    </ul>
+                </div>
+    
+                <div class="line"> <hr /></div>  
+    
+                <div class="professional_summary">
+                    <h3>Professional Summary</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet quam at purus varius bibendum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet quam at purus varius bibendum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet quam at purus varius bibendum.
+                    </p>
+                </div>
+    
+                <div class="line"> <hr /></div>  
+    
+                <div class="work">
+                    <h3>Work History</h3>
+                    <ul class="work-ul">
+                        <li>
+                            <h4>Front-End Developer</h4>
+                            <p>Example Company, New York</p>
+                            <p>Responsible for developing and maintaining user interfaces...</p>
+                        </li>
+                        <li>
+                            <h4>Front-End Developer</h4>
+                            <p>Example Company, New York</p>
+                            <p>Responsible for developing and maintaining user interfaces...</p>
+                        </li>
+                        <li>
+                            <h4>Front-End Developer</h4>
+                            <p>Example Company, New York</p>
+                            <p>Responsible for developing and maintaining user interfaces...</p>
+                        </li>
+                        <!-- Add more work history items as needed -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+    </html>
+    
+    `;
+  };
+
+  const handleResume = async () => {
+    setLoading(true);
+    setError("");
+
+    const axiosConfig = {
+      responseType: "arraybuffer",
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "https://whihtmltopdf.onrender.com/convertToPdf",
+        { htmlContent: getHTML() },
+        axiosConfig
+      );
+
+      setLoading(false);
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "lizmy.pdf");
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+
+
+
 
 
   return (
-    <div  className={style.main}>
-           <div
-        className={style.left_section}
-        style={{ backgroundColor: color, color: "white" }}
-      >
-        <div>
-          <div className={style.img_container}>
-            <div
-              className={style.img_box}
-              style={{ height: imgSize, width: imgSize }}
-            >
-              {croppedImage ? (
-                <img src={croppedImage} alt="dp" />
-              ) : (
-                <img src={formData?.resume?.profilePicture?.url} alt="dp" />
-              )}
-            </div>
-          </div>
-
-          <div className={style.info_box}>
-            <p>
-              <AiOutlineMail />
-              {formData?.resume?.contact?.email}
-            </p>
-            <p>
-              <AiOutlinePhone />
-              {formData?.resume?.contact?.phone}
-            </p>
-            <p>
-              <CiLocationOn />
-              {formData?.resume?.address?.address},
-              {formData?.resume?.address?.postalCode}
-            </p>
-          </div>
-        </div>
-        <br />
-        <hr />
-        <div className={style.education}>
-          <h2>Education</h2>
-          {formData?.resume?.education?.map((item, id) => (
-            <div key={id}>
-              <h3>{item?.collegeName}</h3>
-              <p>{item?.degree}</p>
-              <p>
-                {item?.startYear} - {item?.endYear}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className={style.right_section}>
-        <div className={style.heading} style={{ backgroundColor: color2 }}>
-          <h1
-            style={{ color: color3, fontFamily: fontStyle, fontSize: fontSize }}
-          >
-            {formData?.resume?.name}
-          </h1>
-        </div>
-
-        <div className={style.certifications}>
-          <h2>CERTIFICATIONS</h2>
-          <ul>
-            {formData?.resume?.certifications?.map((item, id) => (
-              <li key={id}>
-                <h5>{item?.title}</h5>
-                <p>Organization: {item?.issuingOrganization}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <br />
-        <hr />
-
-        <div className={style.skills}>
-          <h2>SKILLS</h2>
-          <ul>
-            {formData?.resume?.skillsAndLevel?.map((item, id) => (
-              <li key={id}>{item?.skills}</li>
-            ))}
-          </ul>
-        </div>
-        <br />
-        <hr />
-
-        <div className={style.professional_summary}>
-          <h2>PROFESSIONAL SUMMARY</h2>
-          <p>{formData?.resume?.summary}</p>
-        </div>
-        <br />
-        <hr />
-
-        <div className={style.work}>
-          <h2>WORK HISTORY</h2>
-          <ul className={style.work_history}>
-            {formData?.resume?.work?.map((item, id) => (
-              <li key={id}>
-                <h3>{item?.title}</h3>
-                <p>
-                  {item?.company} , {item?.location}
-                </p>
-                <p>{item?.description}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div>
+    <button onClick={handleResume}>Download</button>
+    <br />
+    {loading && <p>Loading...</p>}
+    {error && <p style={{ color: "red" }}>{error}</p>}
+    <PDFRenderer htmlContent={getHTML()} />
+  </div>
   );
-}
+};
 
 export default Template_1;
